b6f7b34ba10c0dfea34a2688a51a81ee48cc8335
Jason Goetz <jason.goetz@gmail.com>
2022-08-13 22:05:17 -0700

    Added a rule about betting bonuses

--- src/app/components/Rules.tsx
+++ src/app/components/Rules.tsx
@@ -13,26 +13,40 @@ export const Rules: React.FC = () => {
     const { leagueStore } = useGlobalStores();
     const league = leagueStore.league!;
 
+    const startingAccountRule = `You start out with an imaginary $${league.startingAccount} in your account. Whoever has
+                the most imaginary money in their account at the end of the season wins.`;
+
+    const betCountMaxRules = `You can make up to ${league.weeklyBetCountMax} bets per week, but don't have to make
+  any bets.`;
+
+    const accountRatioRule = `You cannot bet more than ${league.weeklyBetAccountRatio * 100}% of the stash you started the
+                week with. If you come into the week with $100, then the most you can spread over your bets is
+                $${100 * league.weeklyBetAccountRatio}.`;
+
+    const bettingBonusRule = `For every bet you win, you get a ${league.moneyline - 100}% bonus on top of your 
+                winnings. If you win on a $100 bet, you get $${league.moneyline}. Bet often.`
+
+    const lineMoveWarning = `Lines move, so please be aware that you are locked into whatever point spread or over/under
+                you initially wager on.`
+
+    const rules: string[] = [];
+    rules.push(startingAccountRule);
+    rules.push(betCountMaxRules);
+    if (league.weeklyBetAccountRatio < 1) {
+        rules.push(accountRatioRule);
+    }
+    if (league.moneyline > 100) {
+        rules.push(bettingBonusRule);
+    }
+    rules.push(lineMoveWarning);
+
     return (
         <div>
-            <div style={ruleStyle}>
-                1. You start out with an imaginary ${league.startingAccount} in your account. Whoever has
-                the most imaginary money in their account at the end of the season wins.
+          {rules.map((rule, index) => (
+            <div style={index === rules.length - 1 ? lastRuleStyle: ruleStyle}>
+              {index + 1}. {rule}
             </div>
-            <div style={ruleStyle}>
-                2. You can make up to {league.weeklyBetCountMax} bets per week, but don't have to make
-                any bets.
-            </div>
-            <div style={ruleStyle}>
-                3. You cannot bet more than {league.weeklyBetAccountRatio * 100}% of the stash you started the
-                week with. If you come into the week with $100, then the most you can spread over your bets is
-                ${100 * league.weeklyBetAccountRatio}.
-            </div>
-            <div style={lastRuleStyle}>
-                4. Lines move, so please be aware that you are locked into whatever point spread or over/under
-                you
-                initially wager on.
-            </div>
+          ))}
         </div>
     )
 };
